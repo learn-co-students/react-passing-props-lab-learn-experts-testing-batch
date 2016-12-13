77fc98e2462bc467259fc221840ac6fd01504551
Enoch2k2 <enoch2k2@hotmail.com>
2016-12-12 18:25:03 -0600

    Done.

--- components/App.js
+++ components/App.js
@@ -2,10 +2,52 @@ const React = require('react');
 
 const FruitBasket = require('./FruitBasket');
 
-const App = () => {
-  return (
-    <FruitBasket />
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      fruit: [],
+      filters: [],
+      selectedFilter: null
+    };
+
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+    this.fetchFilters = this.fetchFilters.bind(this);
+    this.fetchFruit = this.fetchFruit.bind(this);
+  }
+
+  componentWillMount() {
+    this.fetchFilters();
+    this.fetchFruit();
+  }
+
+  fetchFilters() {
+    fetch('/api/fruit_types')
+      .then(res => res.json())
+      .then(filters => this.setState({filters: filters}));
+  }
+
+  fetchFruit() {
+    fetch('/api/fruit')
+      .then(res => res.json())
+      .then(fruit => this.setState({ fruit: fruit}));
+  }
+
+  handleFilterChange(e) {
+    console.log('new filter: ', e.target.value);
+    this.setState({ selectedFilter: e.target.value });
+  }
+
+  render(){
+
+    return (
+      <FruitBasket handleFilterChange={this.handleFilterChange}
+        selectedFilter={this.state.selectedFilter}
+        filters={this.state.filters}
+        fruit={this.state.fruit} />
+    )
+  }
 }
 
 module.exports = App;
