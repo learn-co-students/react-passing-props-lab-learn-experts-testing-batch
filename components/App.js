cb4d80e04a654360a17c1e7080e5dbd1a97ed85c
pajamaw <pjpeterww@gmail.com>
2017-02-11 23:25:32 -0500

    Done.

--- components/App.js
+++ components/App.js
@@ -2,10 +2,52 @@ const React = require('react');
 
 const FruitBasket = require('./FruitBasket');
 
-const App = () => {
-  return (
-    <FruitBasket />
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      filters: [],
+      selectedFilter: null,
+      fruit: []
+    }
+    this.fetchFilters = this.fetchFilters.bind(this)
+    this.fetchFruit = this.fetchFruit.bind(this)
+    this.updateFilter = this.updateFilter.bind(this)
+  }
+  componentDidMount(){
+    this.fetchFilters();
+    this.fetchFruit();
+  }
+  updateFilter(e){
+    this.setState({
+      selectedFilter: e.target.value
+    })
+  }
+
+  fetchFilters(){
+    fetch('/api/fruit_types')
+      .then(res => res.json())
+      .then(filts => this.setState({
+        filters: filts
+        }))
+  }
+  fetchFruit(){
+    fetch('/api/fruit')
+      .then(res => res.json())
+      .then(frt => this.setState({
+          fruit: frt
+        }))
+  }
+  render(){
+    return (
+      <FruitBasket
+        filters={this.state.filters}
+        fruit={this.state.fruit}
+        currentFilter={this.state.selectedFilter}
+        updateFilterCallback={this.updateFilter}
+        />
+    );
+  }
 }
 
 module.exports = App;
